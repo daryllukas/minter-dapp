70fd6b6f4b3f4f096023101c83c512628fd60b23
Daryl Lukas <hello@daryllukas.me>
2022-06-20 14:48:10 +0200

    update logs

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -235,7 +235,7 @@ async function loadInfo() {
   
   pricePerMint.innerText = `${price} ${priceType}`;
   maxPerMint.innerText = `${info.deploymentConfig.tokensPerMint}`;
-  console.log('deploymentConfig', info.deploymentConfig);
+  console.log('deploymentConfig', info);
   totalSupply.innerText = `${info.deploymentConfig.maxSupply}`;
   mintInput.setAttribute("max", info.deploymentConfig.tokensPerMint);
 
